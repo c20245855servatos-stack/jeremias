d26258e7c294bbdf371d703f5690e65632c22e2f
c20245855servatos-stack <c20245855.servatos@csav.edu.ph>
2026-09-04 16:44:13 +0800

    querySelector

--- script.js
+++ script.js
@@ -23,13 +23,14 @@ if (Array.isArray(names))
    names=names.join(", ");
     
   return `${greetings} sa enyu ${names}`;
-
-
 }
- 
 console.log(greet("Mayong aga", ["Jeremias","John", "kim","jhonny", "leopard"]));
 
+const heading = document.querySelector("h1");
+console.log(heading);
 
+const contactHeading = document.querySelector("#contact h2");
+console.log(contactHeading)
 
-
-
+const servicesHeading = document.querySelector("#services h2");
+console.log(servicesHeading)
