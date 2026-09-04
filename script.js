a4c26f9e27dc4861449a000b45526309463b4b26
c20245855servatos-stack <c20245855.servatos@csav.edu.ph>
2026-09-04 14:50:11 +0800

    variables (let and cons)

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 console.log("Hello, World!");
 console.log ("From the script.js file");
+
+const myName = "Jeremias";
+let age= 20;
+let number = "09519742640";
+let address = "Purok agtongtong, Brgy. Tortosa, Manapala";
+let siksseben = 67;
+
+
+
+
+
+
+console.log (`Name : ${myName}`);
+console.log (`Age : ${age}`);
+console.log (`Number : ${number}`);
+console.log (`Address : ${address}`);
+console.log (`${siksseben}`);
+
